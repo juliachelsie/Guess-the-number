dff7f22edfd707ccf16fad05265e1b394c1c2373
juliachelsie <>
2023-09-25 15:57:39 +0000

    Add while loop with if elif else

--- run.py
+++ run.py
@@ -12,3 +12,15 @@ Stores the guesses that the user makes
 
 guess = 0
 
+while guess != number:
+    guess = int(input("Make a Guess:"))
+
+    if (guess < number):
+        print("Sorry you guessed to low, guess again")
+    elif (guess > number):
+        print("Sorry you guessed to high, guess again!")
+    else:
+        print("Woho you guessed correct!")
+
+
+
