7e7ffaefe9c4936092f93f5f830da0b42cf26ca3
juliachelsie <>
2023-09-26 10:41:38 +0000

    Made variable guess a function and called it

--- run.py
+++ run.py
@@ -1,26 +1,24 @@
 import random
 
+
+def guess(x):
+    """
+    Function that generates a random number between 1 and 100
+    """
+    number = random.randint(1, x)
+    guess = 0
+    while guess != number:
+        guess = int(input(f"Make a guess between 1 and {x}:\n"))
+
+        if (guess < number):
+            print("Sorry you guessed to low, guess again!")
+        elif (guess > number):
+            print("Sorry you guessed to high, guess again!")
+        else:
+            print(f"Woho you guessed correct, it was {number}!")
+
+
 """
-Generates a random number between 1 and 100
+Calling the functions
 """
-
-number = random.randint(1,100)
-
-"""
-Stores the guesses that the user makes
-"""
-
-guess = 0
-
-while guess != number:
-    guess = int(input("Make a Guess:"))
-
-    if (guess < number):
-        print("Sorry you guessed to low, guess again")
-    elif (guess > number):
-        print("Sorry you guessed to high, guess again!")
-    else:
-        print("Woho you guessed correct!")
-
-
-
+guess(100)
